fbeb16123dad1011d02e905028628534e8ea0568
AnaSofia2002 <>
2022-05-25 22:17:49 -0500

    parcial2

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDjo_BkG-jbl_YkftH1mXt55J2PV4ootws",
+    authDomain: "parcial2-893ad.firebaseapp.com",
+    projectId: "parcial2-893ad",
+    storageBucket: "parcial2-893ad.appspot.com",
+    messagingSenderId: "522553892216",
+    appId: "1:522553892216:web:9e854d5f0e18e6413e3c8d",
+    measurementId: "G-9RS9KZ9CL4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
